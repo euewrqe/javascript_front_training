d12374238eb660c92b141ad31fa51e0bf7398954
euewrqe <euewrqe@gmail.com>
2019-12-16 20:23:48 +0900

    step 1

--- js贪吃蛇/lib.js
+++
@@ -1,8 +0,0 @@
-﻿function randomInt(first, step, limit) {
-    item = parseInt(Math.random() * step + first);
-    if (item % limit != 0) {
-        item-=item % limit
-    }
-    
-    return item;
-}
